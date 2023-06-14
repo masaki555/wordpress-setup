bb6dd3b64a790602696ee973ee148a78474587ff
masaki.obana <masaki.obana@oit.ac.jp>
2023-06-14 23:40:52 +0900

    2023年版

---
+++ factorio/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo snap install docker
+
+sudo docker-compose up -d
--- wordpress/install.sh
+++ wordpress/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo yum install -y docker
-sudo service docker start
+sudo snap install docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
